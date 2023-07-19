da4a10d52b7029f1802d12cfeb895e26cf202603
TK17250 <TXK285@gmail.com>
2023-07-19 23:10:22 +0700

    text import model file obj mtl & file image

--- src/script.js
+++ src/script.js
@@ -28,56 +28,60 @@ scene.add(camera)
 const dracoLoader = new DRACOLoader()
 dracoLoader.setDecoderPath('draco/')
 
-// const gltfLoader = new GLTFLoader()
-// gltfLoader.setDRACOLoader(dracoLoader)
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
 
 scene.background = new THREE.Color(0xffffff);
 
-// var light = new THREE.AmbientLight( 0xededed );
-// light.intensity = 1.3;
-// scene.add(light);
+let light = new THREE.AmbientLight( 0xededed );
+light.intensity = 1.3;
+scene.add(light);
 
 var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
 scene.add( directionalLight );
 
-// gltfLoader.load(
-//     'Umbelra.glb',
-//     (gltf) =>
-//     {
-//         gltf.scene.rotation.x = 0.7;
-//         gltf.scene.rotation.y = 0.5;
-//         gltf.scene.rotation.z = 5.4;
-//         scene.add(gltf.scene)
-//     }
-// )
+// ใช้อันนี้ไปก่อน
+gltfLoader.load(
+    'Umbelra.glb',
+    (gltf) =>
+    {
+        gltf.scene.rotation.x = 0.7;
+        gltf.scene.rotation.y = 0.5;
+        gltf.scene.rotation.z = 5.4;
+        scene.add(gltf.scene)
+    }
+)
 
-const textureLoader = new THREE.TextureLoader();
-const mtlLoader = new MTLLoader();
+// const textureLoader = new THREE.TextureLoader();
+// const mtlLoader = new MTLLoader();
 
-mtlLoader.load('1.mtl', (mtl) => {
-    mtl.preload();
+// textureLoader.load('1.jpg', (image) => {
+//     const texture = new THREE.Texture();
+//     texture.image = image;
+//     texture.needsUpdate = true;
 
-    const objLoader = new OBJLoader();
-    objLoader.setMaterials(mtl);
+//     const material = new THREE.MeshPhysicalMaterial({ map: image });
+//     material.side = THREE.DoubleSide;
 
-    textureLoader.load('1.jpg', (texture) => {
-        const material = new THREE.MeshBasicMaterial({ map: texture });
-        texture.wrapS = THREE.RepeatWrapping;
-        texture.wrapT = THREE.RepeatWrapping;
-        texture.repeat.set( 4, 4 );
+//     mtlLoader.load('1.mtl', (mtl) => {
+//         mtl.preload();
 
-        objLoader.load('1.obj', (obj) => {
-            obj.traverse((child) => {
-                if (child instanceof THREE.Mesh) {
-                    child.material = material;
-                }
-            });
+//         const objLoader = new OBJLoader();
+//         objLoader.setMaterials(mtl);
 
-            scene.add(obj);
-        });
-    });
-});
+//         objLoader.load('1.obj', (obj) => {
+//             obj.traverse((child) => {
+//                 if (child instanceof THREE.Mesh) {
+//                     child.material = material;
+//                     mtl.materials = material;
+//                 }
+//             });
 
+//             scene.add(obj);
+            
+//         });
+//     });
+// });
 
 const cursor ={x:0, y:0}
 window.addEventListener('mousemove', (event) =>
